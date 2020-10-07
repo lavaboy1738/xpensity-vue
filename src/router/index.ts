582db2f064914643f7cc81546897151fb616bf4f
Ronnie Feng <ronnie@ronniefeng.com>
2020-10-07 19:12:22 -0400

    adding 404 page

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from "@/views/Home.vue";
 import Money from "@/views/Money.vue";
 import Tags from "@/views/Tags.vue";
 import Stats from "@/views/Stats.vue";
+import NotFound from "@/views/NotFound.vue";
 
 Vue.use(VueRouter)
 
@@ -25,6 +26,10 @@ const router = new VueRouter({
     {
       path: "/stats",
       component: Stats
+    },
+    {
+      path: "*",
+      component: NotFound
     }
   ]
 })
---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        stoopid
+        <div>
+            <router-link to="/" >back to homepage</router-link>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"NotFound"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
