052ed271e965f31079ddb34d8b00177c859ec11e
Ronnie Feng <ronnie@ronniefeng.com>
2020-10-17 22:23:21 -0400

    adding edit tag component

--- src/router/index.ts
+++ src/router/index.ts
@@ -24,7 +24,7 @@ const router = new VueRouter({
       component: Tags
     },
     {
-      path: "/tags/edit",
+      path: "/tags/edit/:id",
       component: EditTag
     },
     {
--- src/views/EditTag.vue
+++ src/views/EditTag.vue
@@ -7,9 +7,20 @@
 <script lang="ts">
     import Vue from 'vue'
     import {Component} from "vue-property-decorator";
+    import tagList from "@/models/taglist.model";
     @Component
     export default class EditTag extends Vue{
-
+        created(){
+            const id = this.$route.params.id
+            tagList.fetch();
+            const savedTags = tagList.data;
+            const tag = savedTags.filter(tag=> tag.id === id)[0]
+            if(tag){
+                console.log(tag);
+            }else{
+                this.$router.replace("/404")
+            }
+        }
     }
 </script>
 
